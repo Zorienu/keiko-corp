b3c1b41364fd4c9795d9fd66da7b91ff3ebf35f3
Jasyd Caballero <jasydavid45@gmail.com>
2021-02-17 22:16:20 -0500

    loading css with js file at the bottom of index.html file

---
+++ js/createStyleSheet.js
@@ -0,0 +1,17 @@
+const addStyleSheet = (src) => {
+  if (document.createStyleSheet) {
+    document.createStyleSheet(src);
+  } else {
+    const styleSheet = document.createElement("link");
+    styleSheet.href = src;
+    styleSheet.type = "text/css";
+    styleSheet.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(styleSheet);
+  }
+};
+
+window.onload = () => {
+  addStyleSheet("css/animate.css");
+  addStyleSheet("css/owl.carousel.css");
+  addStyleSheet("css/owl.transitions.css");
+};
